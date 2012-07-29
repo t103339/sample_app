2bd854850fca94358a3acc689c2ad4e95750138d
MARV <coder339@aol.com>
2012-07-29 12:25:00 -0300

    mod to application.html.erb and home.html.erb

--- Gemfile
+++ Gemfile
@@ -44,6 +44,10 @@ group :test do
   gem 'guard-spork', '0.3.2'  
   gem 'spork', '0.9.0'
   gem 'launchy', '2.1.0'
+  gem 'rb-fchange', '0.0.5'
+  gem 'rb-notifu', '0.0.4'
+  gem 'win32console', '1.3.0.beta3'
+  #gem 'win32console', '1.3.0.beta3-x86-mingw32'
   # gem 'rb-fsevent', '0.9.1', :require => false
   # gem 'growl', '1.0.3'
 end  
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,14 +6,22 @@ describe "StaticPages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', 
+                        :text => 'Sample App')
     end
 
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "#{base_title} | Home")
+      page.should have_selector('title', 
+                        :text => "Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', 
+                        :text => '| Home')
     end
 
   end
@@ -50,6 +58,11 @@ describe "StaticPages" do
     it "should have the content 'Contact'" do
       visit '/static_pages/contact'
       page.should have_content('Contact')
+    end
+
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => "Contact")
     end
 
     it "should have the right title" do
